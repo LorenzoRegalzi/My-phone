e00d25d5ce9b8923e6371cb962551bc7ba1acc6d
LorenzoRegalzi <lorenzo.regalzi@gmail.com>
2020-08-19 11:06:41 +0200

    start

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Welcome</title>
+    </head>
+    <body>
+        <h1>hi</h1>
+        <a href="">area login</a>
+        <h2> present of products</h2>
+        <p> here best post</p>
+    </body>
+</html>
